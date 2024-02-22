a5ebaed72b0830a12f5c7d958b0c789547e17697
Félix Mauricio Vargas Hincapié <felix.vargas@ceinfes.com>
2024-02-22 15:25:42 -0500

    se añaden cors

--- app.py
+++ app.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 from config.db import Base, engine
 from pensar.controllers.controllers import router_pensar
 import pensar.models.models as pen
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 pen.Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router_pensar, prefix="/Pruebapensar", tags=["componente"])
