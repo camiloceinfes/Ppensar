261d92c431656c6c9d9a82fcd25e650a85b1c739
Esteban Correa <estebancorrea405@gmail.com>
2024-02-23 12:23:10 -0500

    Agregando seguridad a la API mediante JWT

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from config.db import Base, engine
 from pensar.controllers.controllers import router_pensar
 import pensar.models.models as pen
 from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
 
 pen.Base.metadata.create_all(bind=engine)
 
